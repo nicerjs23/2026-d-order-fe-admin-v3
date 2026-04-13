7af30f07324ab1448e2429ff2609b3d6460d91c5
gn00py48 <gn00py48@gmail.com>
2026-04-13 12:41:43 +0900

    fix : table view page height fix

--- src/pages/tableView/TableViewPage.styled.ts
+++ src/pages/tableView/TableViewPage.styled.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 export const PageWrapper = styled.div`
     width: 100%;
+    /* min-height: calc(var(--vh, 1vh) * 100);      */
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: center;
-    background-color: ${({theme}) => theme.colors.Bg}; 
-
+    background-color: ${({theme}) => theme.colors.Bg};
+    overflow: hidden; /* 전체 페이지 스크롤 방지 */
 `;
